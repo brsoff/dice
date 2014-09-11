e4bea2385e5353495a914702a21bde1d5b0dd218
brsoff <brsoff@gmail.com>
2014-09-11 08:47:24 -0400

    Forgot what i did here

--- dice.rb
+++ dice.rb
@@ -1,39 +1,5 @@
 require_relative 'models'
 
-puts "Welcome to Dice. Please enter a name: "
-name = gets.chomp
-
-game = Game.new
-player = Player.new(name)
-computer = Computer.new
-
-until computer.turn < 1
-  computer.roll(computer.turn)
-end
-
-puts "Hello #{ player.name }. The computer scored a #{ computer.score }. Press 'r' to roll the dice or 'q' to quit."
-input = gets.chomp
-
-while game.in_progress
-  case input
-    when 'r'
-      roll = player.roll(player.turn)
-
-      if player.score > computer.score
-        game.in_progress = false
-        game.game_over(player.score, computer.score)
-      elsif player.turn > 0
-        puts "Your current score is #{ player.score }. Press 'r' to roll again or 'q' to quit."
-        input = gets.chomp
-      else
-        game.in_progress = false
-        game.game_over(player.score, computer.score)
-      end
-    when 'q'
-      puts "Goodbye!"
-      game.in_progress = false
-    else
-     puts "D'oh! Please press either 'r' or 'q'"
-     input = gets.chomp 
-  end
-end
+gameplay = GamePlay.new
+gameplay.greet
+gameplay.begin
--- models.rb
+++ models.rb
@@ -1,3 +1,53 @@
+class GamePlay
+  attr_accessor :game, :player, :computer
+
+  def initialize
+    @game = Game.new
+    @player = Player.new(get_name)
+    @computer = Computer.new
+  end
+
+  def get_name
+    puts "Welcome to Dice. Please enter a name: "
+    gets.chomp
+  end
+
+  def greet
+    puts "Hello #{ @player.name }"
+  end
+
+  def begin
+    @computer.roll(@computer.turn)  until @computer.turn < 1
+
+    puts "The computer scored a #{ @computer.score }. Press 'r' to roll the dice or 'q' to quit."
+    input = gets.chomp
+
+    while @game.in_progress
+      case input
+        when 'r'
+          @player.roll(@player.turn)
+
+          if @player.score > @computer.score
+            @game.in_progress = false
+            @game.game_over(@player.score, @computer.score)
+          elsif @player.turn > 0
+            puts "Your current score is #{ @player.score }. Press 'r' to roll again or 'q' to quit."
+            input = gets.chomp
+          else
+            @game.in_progress = false
+            @game.game_over(@player.score, @computer.score)
+          end
+        when 'q'
+          puts "Goodbye!"
+          @game.in_progress = false
+        else
+         puts "D'oh! Please press either 'r' or 'q'"
+         input = gets.chomp
+      end
+    end
+  end
+end
+
 class Game
   attr_accessor :in_progress
 
@@ -16,15 +66,14 @@ class Game
   end
 end
 
-class Die 
+class Die
   def self.roll
     [1, 2, 3, 4, 5, 6].sample
   end
 end
 
 class Player
-  attr_reader :name
-  attr_accessor :score, :turn, :current_roll
+  attr_accessor :name, :score, :turn, :current_roll
 
   def initialize(name = nil)
     @name = name
@@ -34,17 +83,17 @@ class Player
   end
 
   def roll(num)
-    num.times { @current_roll << Die.roll } 
-    
+    num.times { @current_roll << Die.roll }
+
     puts "You rolled:"
     @current_roll.each_with_index { |n, i| puts "(#{ i + 1 }) #{ n }" }
-   
+
     if @turn == 1
-      choices = [@current_roll.first] 
+      choices = [@current_roll.first]
     else
       puts "Which one(s) would you like to keep? (separate with commas)"
       input = gets.chomp.split(",")
-      
+
       choices = []
       input.each { |n| @current_roll[n.to_i - 1] == 3 ? choices << 0 : choices << @current_roll[n.to_i - 1] }
     end
@@ -55,9 +104,9 @@ class Player
   def tally(choices)
     total = 0
     choices.each { |n| total += n }
-    
+
     @score += total
-    @current_roll = []   
+    @current_roll = []
 
     @turn -= choices.count
   end
